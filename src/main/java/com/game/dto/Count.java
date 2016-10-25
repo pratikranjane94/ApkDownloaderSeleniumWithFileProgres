9cca0cbfec28b4cc9c9cbeb5194cdb329915c75b
PRATIK <pratikranjane94@gmail.com>
2016-10-25 11:03:54 +0530

    configuration changed

--- src/main/java/com/game/dto/Count.java
+++ src/main/java/com/game/dto/Count.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Count {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	private int key;
 	@Column
 	private int id;
